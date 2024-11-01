03c9673a73476f7b79e4cf893397de208288627d
Martin Poulsen <martin.poulsen@elitappar.com>
2024-11-01 09:13:01 +0100

    Initial Commit

---
+++ SplitViewSample/SplitViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitViewSampleApp.swift
+//  SplitViewSample
+//
+//  Created by Martin Poulsen on 2024-11-01.
+//
+
+import SwiftUI
+
+@main
+struct SplitViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
